4ff2bdb4e1592e2753a12f6b8bb1ade95cd1ad10
ammusani <ammusani@users.noreply.github.com>
2019-01-27 13:49:47 +0530

    Create lab3_printingJob.cpp

---
+++ lab3/assignment/lab3_printingJob.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    int n;
+    int m;
+    while(t--) {
+        scanf("%d%d", &n, &m);
+        int a[n];
+        int k;
+        for(int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        k = a[m];
+        int time = 0;
+        int above = 0;
+        for(int i = 0; i < n; i++) {
+            if()
+        }
+    }
+}
